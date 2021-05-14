b87c42bf0e956f0602a1bb2692f35558bafebfb2
Gustavo Stingelin Cardoso <gustavo.stingelin.c@outlook.com>
2021-05-13 22:56:38 -0300

    Query Params and Paginated Search

---
+++ 15 RestWithSpringBoot Query Params and Paginated Search/src/main/java/br/com/stingelin/services/PersonServices.java
@@ -0,0 +1,69 @@
+package br.com.stingelin.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import br.com.stingelin.converter.DozerConverter;
+import br.com.stingelin.data.model.Person;
+import br.com.stingelin.data.vo.v1.PersonVO;
+import br.com.stingelin.exception.ResourceNotFoundException;
+import br.com.stingelin.repository.PersonRepository;
+
+@Service
+public class PersonServices {
+	
+	@Autowired
+	PersonRepository repository;
+	
+	public PersonVO create(PersonVO person) {
+		var entity = DozerConverter.parseObject(person, Person.class);
+		return DozerConverter.parseObject(repository.save(entity), PersonVO.class);
+	}
+	
+	public Page<PersonVO> findAll(Pageable pageable) {
+		var page = repository.findAll(pageable);
+		return page.map(this::convertToPersonVO);
+	}
+
+	public Page<PersonVO> findPersonByName(String firstName, Pageable pageable) {
+		var page = repository.findPersonByName(firstName, pageable);
+		return page.map(this::convertToPersonVO);
+	}
+
+	private PersonVO convertToPersonVO(Person entity) {
+		return DozerConverter.parseObject(entity, PersonVO.class);
+	}
+	
+	public PersonVO findById(Long id) {
+		var entity = repository.findById(id)
+				.orElseThrow(() -> new ResourceNotFoundException("No records found for this Id"));
+		return DozerConverter.parseObject(entity, PersonVO.class);
+	}
+	
+	public PersonVO update(PersonVO person) {
+		var entity = repository.findById(person.getKey())
+				.orElseThrow(() -> new ResourceNotFoundException("No records found for this Id"));
+		entity.setFirstName(person.getFirstName());
+		entity.setLastName(person.getLastName());
+		entity.setAddress(person.getAddress());
+		entity.setGender(person.getGender());
+		return DozerConverter.parseObject(repository.save(entity), PersonVO.class);
+	}	
+	
+	@Transactional
+	public PersonVO disablePerson(Long id) {
+		repository.disablePerson(id);
+		var entity = repository.findById(id)
+				.orElseThrow(() -> new ResourceNotFoundException("No records found for this Id"));
+		return DozerConverter.parseObject(entity, PersonVO.class);
+	}
+
+	public void delete(Long id) {
+		Person entity = repository.findById(id)
+				.orElseThrow(() -> new ResourceNotFoundException("No records found for this Id"));
+		repository.delete(entity);
+	}
+}
