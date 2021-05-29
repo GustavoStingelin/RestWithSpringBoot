59cf38af4a3df9be747360f797603c8ab8fdc213
Gustavo Stingelin Cardoso <gustavo.stingelin.c@outlook.com>
2021-05-29 12:07:52 -0300

    working with docker

---
+++ 17 RestWithSpringBoot Docker/API/src/main/java/br/com/stingelin/service/BookService.java
@@ -0,0 +1,55 @@
+package br.com.stingelin.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+import br.com.stingelin.converter.DozerConverter;
+import br.com.stingelin.data.model.Book;
+import br.com.stingelin.data.vo.v1.BookVO;
+import br.com.stingelin.exception.ResourceNotFoundException;
+import br.com.stingelin.repository.BookRepository;
+
+@Service
+public class BookService {
+	
+	@Autowired
+	BookRepository repository;
+	
+	public BookVO create(BookVO book) {
+		var entity = DozerConverter.parseObject(book, Book.class);
+		return DozerConverter.parseObject(repository.save(entity), BookVO.class) ;
+	}
+	
+	public Page<BookVO> findAll(Pageable pageable) {
+		var page = repository.findAll(pageable);
+		return page.map(this::convertToVO);
+	}
+	
+	private BookVO convertToVO(Book entity) {
+		return DozerConverter.parseObject(entity, BookVO.class);
+	}
+
+	public BookVO findById(Long id) {
+		var entity = repository.findById(id)
+				.orElseThrow(() -> new ResourceNotFoundException("No records found for this Id"));
+		return DozerConverter.parseObject(entity, BookVO.class);
+	}
+	
+	public BookVO update(BookVO book) {
+		Book entity = repository.findById(book.getKey())
+				.orElseThrow(() -> new ResourceNotFoundException("No records found for this Id"));
+		entity.setAuthor(book.getAuthor());
+		entity.setLaunch_date(book.getLaunch_date());
+		entity.setPrice(book.getPrice());
+		entity.setTitle(book.getTitle());
+		return DozerConverter.parseObject(repository.save(entity), BookVO.class);
+	}	
+	
+	public void delete(Long id) {
+		Book entity = repository.findById(id)
+				.orElseThrow(() -> new ResourceNotFoundException("No records found for this Id"));
+		repository.delete(entity);
+	}
+}
